a9554b761b1b9b688a4c9d8b423cc18e062b74a8
Yvonne Ingles <ydingles@hotmail.com>
2015-11-24 21:52:36 -0800

    portfolio

--- js/app.js
+++ js/app.js
@@ -1,13 +1,41 @@
-// Create app
-
-// Configure app
+var myApp = angular.module('myApp', ['ui.router'])
+// Config route provider
+.config(function($stateProvider) {
+    $stateProvider
+  .state('home', {
+    url:'/',
+    templateUrl: 'templates/home.html',
+    controller: 'HomeController',
+  })
+  .state('projects', {
+    url:'/projects',
+    templateUrl: 'templates/projects.html',
+    controller: 'ProjectsController',
+  })
+  .state('about', {
+    url:'/about',
+    templateUrl: 'templates/about.html',
+    controller: 'AboutController',
+  })
+})
 
 // Landing page controller: define $scope.number as a number
-
-
-// About page controller: define $scope.about as a string
-
+.controller('HomeController', function($scope, $http){
+  $http.get('json/home.json').success(function(data) {
+    $scope.homeobject = data;
+  })
+})
 
 // Content controller: define $scope.url as an image
+.controller('ProjectsController', function($scope, $http){
+    $http.get('json/projects.json').success(function(data) {
+    $scope.projectsobject = data;
+  })
+})
 
-
+// About contact controller: define $scope.about as a string
+.controller('AboutController', function($scope, $http){
+    $http.get('json/about.json').success(function(data) {
+    $scope.aboutobject = data;
+  })
+})
