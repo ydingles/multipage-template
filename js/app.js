34091581a2afd5a26be591a8a9facc4e95be6f42
ydingles <ydingles@hotmail.com>
2015-11-25 19:52:24 -0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ var myApp = angular.module('myApp', ['ui.router'])
 .config(function($stateProvider) {
     $stateProvider
   .state('home', {
-    url:'/#',
+    url:'/',
     templateUrl: 'templates/home.html',
     controller: 'HomeController',
   })
